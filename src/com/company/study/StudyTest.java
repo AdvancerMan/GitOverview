b0c5726eaa40ade926435ec965cc7c93d3ec799d
AdvancerMan <advancerman@gmail.com>
2018-03-09 21:18:18 +0700

    Added study and test folders

---
+++ src/com/company/study/StudyTest.java
@@ -0,0 +1,9 @@
+package com.company.study;
+
+import static com.company.study.reverse_a_string.ReverseString.test;
+
+public class StudyTest {
+    public static void main(String[] args) {
+        test();
+    }
+}
---
+++ src/com/company/study/reverse_a_string/ReverseString.java
@@ -0,0 +1,9 @@
+package com.company.reverse_a_string;
+
+import java.util.Scanner;
+
+public class Reverse {
+    public static void main(String[] args) {
+        Scanner scan  = new Scanner(System.in);
+    }
+}
