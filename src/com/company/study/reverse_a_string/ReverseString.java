fa707a90ea53bd570026ad7c37f76b7e3bf5e081
AdvancerMan <advancerman@gmail.com>
2018-03-09 21:23:52 +0700

    Bug fixes

--- src/com/company/study/reverse_a_string/ReverseString.java
+++ src/com/company/study/reverse_a_string/ReverseString.java
@@ -1,9 +1,9 @@
-package com.company.reverse_a_string;
+package com.company.study.reverse_a_string;
 
 import java.util.Scanner;
 
-public class Reverse {
-    public static void main(String[] args) {
+public class ReverseString {
+    public static void test() {
         Scanner scan  = new Scanner(System.in);
     }
 }
